1942e57114eb714fdb3e8956661c465888f66521
Tanush Gupta <tanushgupta.41@gmail.com>
2025-05-26 12:23:36 +0530

    corrected backend connection URL

--- front-end/src/constants/constants.js
+++ front-end/src/constants/constants.js
@@ -1,4 +1,4 @@
 // export const baseURL = "https://task-mate-backend-seven.vercel.app/api/v1";
-// export const baseURL = "https://task-buddy-pied.vercel.app/api/v1";
-export const baseURL = "http://localhost:5050/api/v1";
-export const backendURL = "http://localhost:5050";
+export const baseURL = "https://task-buddy-pied.vercel.app/api/v1";
+// export const baseURL = "http://localhost:5050/api/v1";
+export const backendURL = "https://task-buddy-pied.vercel.app";
